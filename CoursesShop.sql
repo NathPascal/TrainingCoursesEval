29e464b93866042a7f0075e447f0e481dfbdb13d
Nathalie Pascal <nathalie.pascal@fms-ea.com>
2024-10-23 11:57:26 +0200

    second commit

--- CoursesShop.sql
+++ CoursesShop.sql
@@ -1,52 +1,45 @@
 -- Construction BDD --
--- ------------------------------------------------------------------------------
+
 DROP DATABASE IF EXISTS CoursesShop;
 CREATE DATABASE CoursesShop;
 USE CoursesShop;
 
--- Construction de la table des catégories d'articles --
+-- Construction de la table des catégories -- 
 
 CREATE TABLE T_Categories (
-	IdCategory 				INT(4) 		 PRIMARY KEY AUTO_INCREMENT,
-	CatName 				VARCHAR(30)  NOT NULL,
-	Description 			VARCHAR(100) NOT NULL
+	IdCategory INT(4) NOT NULL AUTO_INCREMENT,
+	CatName VARCHAR(30) NOT NULL,
+	CatDescription VARCHAR(100) NOT NULL,
 ) ENGINE = InnoDB;
 
-insert into T_Categories (IdCategory, CatName, Description) values (1 , 'PC' , 'ordinateur portable ou pas');
-insert into T_Categories (IdCategory, CatName, Description) values (2 , 'SmartPhone' , 'Tel derniere generation avec ecran tactile');
-insert into T_Categories (IdCategory, CatName, Description) values (3 , 'Materiel IT' , 'Tout matos informatique');
-insert into T_Categories (IdCategory, CatName, Description) values (4 , 'SE et Logiciel' , 'Systeme dexploitation et autre Software');
+INSERT INTO T_Categories (IdCategory, CatName, CatDescription) VALUES (1, 'Java', 'tout ce qu"il y a à savoir sur Java');
+INSERT INTO T_Categories (IdCategory, CatName, CatDescription) VALUES (2, 'Web', 'Mise en forma page web');
+INSERT INTO T_Categories (IdCategory, CatName, CatDescription) VALUES (3, 'Autre', 'Autre que Java: langage de programmation, frameworks...');
 
-select * from t_categories;
+SELECT * FROM T_Categories;
 
--- -----------------------------------------------------------------------------
--- - Construction de la table des articles en vente                         ---
--- -----------------------------------------------------------------------------
-CREATE TABLE T_Articles (
-	IdArticle				int(4)		PRIMARY KEY AUTO_INCREMENT,
-	Description				varchar(30)	NOT NULL,
-	Brand					varchar(30)	NOT NULL,
-	UnitaryPrice			float(8)	NOT NULL DEFAULT 0,
-	IdCategory				int(4),
-	FOREIGN KEY (IdCategory)	REFERENCES T_Categories(IdCategory)
-) ENGINE = InnoDB;
+-- Construction de la tables des formations -- 
 
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory ) VALUES ( 'Souris'     ,	'Logitoch', 65			, 3 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory ) VALUES ( 'Clavier'    ,	'Microhard', 49.5 		, 3);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory ) VALUES ( 'Systeme d''exploitation',	'Fenetres Vistouille',	150 , 4);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'Tapis souris', 'Chapeau Bleu',5 		, 3);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'Cle USB 8 To', 'Syno', 8 				, 3);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'Laptop'      , 	'PH',	1199        , 1);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'CD x 500'    , 'CETME', 250 			, 3);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'DVD-R x 100' , 'CETME', 99 			, 3);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'DVD+R x 100' , 'CETME', 105			, 3);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'Batterie Laptop', 'PH',	80 			, 3);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'Casque Audio', 'Syno',	55 			, 3);
-INSERT INTO T_Articles ( Description, Brand , IdCategory  ) VALUES ( 'WebCam'      , 	'Logitoch' 							, 3);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'MacBook', 'Apple',	2000 			, 1);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'S10', 'Samsung',	1500				, 2);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'Iphone150', 'Apple',	200000			, 2);
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice , IdCategory  ) VALUES ( 'Office', 'Microsoft',	150 			, 4);
+CREATE TABLE T_Courses (
+	IdCourse INT(4) PRIMARY KEY AUTO_INCREMENT,
+	CourName VARCHAR(30) NOT NULL,
+	CourDescription VARCHAR(100) NOT NULL,
+	Duration INT(11) NOT NULL DEFAULT '0',
+	Format VARCHAR(30) NOT NULL,
+	Price FLOAT(8) NOT NULL DEFAULT '0',
+	IdCategory INT(4),
+	FOREIGN KEY (IdCategory) REFERENCES T_Categories(IdCategory)
+) ENGINE = InnoDB; 
 
+INSERT INTO T_Courses ( CourName, CourDescription, Duration, Format, Price , IdCategory ) VALUES ('Java', 'Java SE8: Syntaxe et POO', 20, 'Presentiel', 100, 1);
+INSERT INTO T_Courses ( CourName, CourDescription, Duration, Format, Price , IdCategory ) VALUES ('Java avancee', 'Exceptions, fichier Jdbc, thread...', 20, 'Presentiel', 100, 1);
+INSERT INTO T_Courses ( CourName, CourDescription, Duration, Format, Price , IdCategory ) VALUES ('Spring', 'Spring core/MVC/Security', 20, 'Presentiel', 100, 1);
+INSERT INTO T_Courses ( CourName, CourDescription, Duration, Format, Price , IdCategory ) VALUES ('PHP frameworks', 'Symphony', 15, 'Distanciel', 80, 3);
+INSERT INTO T_Courses ( CourName, CourDescription, Duration, Format, Price , IdCategory ) VALUES ('C#', 'DotNet Core', 20, 'Distanciel', 100, 3);
+INSERT INTO T_Courses ( CourName, CourDescription, Duration, Format, Price , IdCategory ) VALUES ('HTML', 'Syntaxe', 15, 'Distanciel', 80, 2);
+INSERT INTO T_Courses ( CourName, CourDescription, Duration, Format, Price , IdCategory ) VALUES ('CSS', 'Syntaxe', 15, 'Distanciel', 80, 2);
+INSERT INTO T_Courses ( CourName, CourDescription, Duration, Format, Price , IdCategory ) VALUES ('UML', 'Use case, Diagramme de classe et de séquence', 20, 'Presentiel', 100, 3);
+INSERT INTO T_Courses ( CourName, CourDescription, Duration, Format, Price , IdCategory ) VALUES ('Figma', 'les différentes fonctionnalités', 10, 'Distanciel', 50, 3);
+INSERT INTO T_Courses ( CourName, CourDescription, Duration, Format, Price , IdCategory ) VALUES ('Eclipse', 'les différentes fonctionnalités', 10, 'Distanciel', 50, 3);
 
-SELECT * FROM T_Articles;
+SELECT * FROM  T_Courses;
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-"# TrainingCoursesEval" 
+Le Use Case : voir fichier **UseCaseDiagram.jpg**    
+Le diagramme de classe: voir fichier **ClassDiagram.jpg**  
+Le diagramme de séquence: voir le fichier  
+
+
+**Spécifications fonctionnelles** 
+  
+Application de vente de formations
+
+L’administrateur peut :
+-	Mettre à jour une formation
+-	Supprimer une formation
+-	Ajouter une formation
+-	Ajouter une catégorie de formations
+-	Supprimer une catégorie de formations
+-	Afficher toutes les commandes d’un client
+
+L’utilisateur non connecté peut : 
+-	Afficher toutes les formations disponibles
+-	Afficher toutes les formations par catégorie ou non
+-	Afficher toutes les formations contenant un mot-clé
+-	Afficher toutes les formations étant en présentiel
+-	Afficher toutes les formations étant en distanciel
+-	Constituer un panier de commandes
+-	Ajouter des formations dans le panier
+-	Retirer des formations dans le panier
+-	Créer un compte utilisateur
+-	Se connecter à son compte login/password
+
+L’utilisateur connecté peut :
+-	Afficher toutes les formations disponibles
+-	Afficher toutes les formations par catégorie ou non
+-	Afficher toutes les formations contenant un mot-clé
+-	Afficher toutes les formations étant en présentiel
+-	Afficher toutes les formations étant en distanciel
+-	Constituer un panier de commandes
+-	Ajouter des formations dans le panier
+-	Retirer des formations dans le panier
+-	Passer commande à tout instant
+-	Réaliser plusieurs commandes pour différents clients
+
+Le compte client comprend nom, prénom, email, adresse, numéro de téléphone
